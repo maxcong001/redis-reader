aedba8b5fdbce49c67b0e6db33490f4d0b3c1d10
Max <savagecm@users.noreply.github.com>
2018-08-16 08:10:53 +0800

    aa

--- example/example.cpp
+++ example/example.cpp
@@ -2,64 +2,110 @@
 #include "logger/logger.hpp"
 #include "resp_parser.hpp"
 #include <stdio.h>
+#include <assert.h>
 int main()
 {
     set_log_level(logger_iface::log_level::debug);
+    std::string test;
+    std::pair<size_t, ParseResult> ret;
 
-    std::string test = "+OK\r\naaaa";
-    rasp_parser::process_resp(test, [](char *buf, size_t len) {
+    test = "+OK\r\n";
+    ret = rasp_parser::process_resp(test, [](char *buf, size_t len) {
         __LOG(debug, "buf is : " << (void *)buf << ", len is : " << len);
     });
+    __LOG(debug, "size of string is : " << test.size() << ", size in the return is : " << std::get<0>(ret));
+    assert(test.size() == std::get<0>(ret));
+
     test = "-Error message\r\n";
-    rasp_parser::process_resp(test, [](char *buf, size_t len) {
+    ret = rasp_parser::process_resp(test, [](char *buf, size_t len) {
         __LOG(debug, "buf is : " << (void *)buf << ", len is : " << len);
     });
+    __LOG(debug, "size of string is : " << test.size() << ", size in the return is : " << std::get<0>(ret));
+    assert(test.size() == std::get<0>(ret));
+
     test = ":1000\r\n";
-    rasp_parser::process_resp(test, [](char *buf, size_t len) {
+    ret = rasp_parser::process_resp(test, [](char *buf, size_t len) {
         __LOG(debug, "buf is : " << (void *)buf << ", len is : " << len);
     });
+    __LOG(debug, "size of string is : " << test.size() << ", size in the return is : " << std::get<0>(ret));
+    assert(test.size() == std::get<0>(ret));
+
     test = "$6\r\nfoobar\r\n";
-    rasp_parser::process_resp(test, [](char *buf, size_t len) {
+    ret = rasp_parser::process_resp(test, [](char *buf, size_t len) {
         __LOG(debug, "buf is : " << (void *)buf << ", len is : " << len);
     });
+    __LOG(debug, "size of string is : " << test.size() << ", size in the return is : " << std::get<0>(ret));
+    assert(test.size() == std::get<0>(ret));
+
     test = "$0\r\n\r\n";
-    rasp_parser::process_resp(test, [](char *buf, size_t len) {
+    ret = rasp_parser::process_resp(test, [](char *buf, size_t len) {
         __LOG(debug, "buf is : " << (void *)buf << ", len is : " << len);
     });
+    __LOG(debug, "size of string is : " << test.size() << ", size in the return is : " << std::get<0>(ret));
+    assert(test.size() == std::get<0>(ret));
+
     test = "$-1\r\n";
-    rasp_parser::process_resp(test, [](char *buf, size_t len) {
+    ret = rasp_parser::process_resp(test, [](char *buf, size_t len) {
         __LOG(debug, "buf is : " << (void *)buf << ", len is : " << len);
     });
+    __LOG(debug, "size of string is : " << test.size() << ", size in the return is : " << std::get<0>(ret));
+    assert(test.size() == std::get<0>(ret));
+
     test = "*0\r\n";
-    rasp_parser::process_resp(test, [](char *buf, size_t len) {
+    ret = rasp_parser::process_resp(test, [](char *buf, size_t len) {
         __LOG(debug, "buf is : " << (void *)buf << ", len is : " << len);
     });
+    __LOG(debug, "size of string is : " << test.size() << ", size in the return is : " << std::get<0>(ret));
+    assert(test.size() == std::get<0>(ret));
+
     test = "*2\r\n$3\r\nfoo\r\n$3\r\nbar\r\n";
-    rasp_parser::process_resp(test, [](char *buf, size_t len) {
+    ret = rasp_parser::process_resp(test, [](char *buf, size_t len) {
         __LOG(debug, "buf is : " << (void *)buf << ", len is : " << len);
     });
+    __LOG(debug, "size of string is : " << test.size() << ", size in the return is : " << std::get<0>(ret));
+    assert(test.size() == std::get<0>(ret));
+
     test = "*3\r\n:1\r\n:2\r\n:3\r\n";
-    rasp_parser::process_resp(test, [](char *buf, size_t len) {
+    ret = rasp_parser::process_resp(test, [](char *buf, size_t len) {
         __LOG(debug, "buf is : " << (void *)buf << ", len is : " << len);
     });
+    __LOG(debug, "size of string is : " << test.size() << ", size in the return is : " << std::get<0>(ret));
+    assert(test.size() == std::get<0>(ret));
 
     test = "*5\r\n:1\r\n:2\r\n:3\r\n:4\r\n$6\r\nfoobar\r\n";
-    rasp_parser::process_resp(test, [](char *buf, size_t len) {
+    ret = rasp_parser::process_resp(test, [](char *buf, size_t len) {
         __LOG(debug, "buf is : " << (void *)buf << ", len is : " << len);
     });
-    test = "*-1\r\n";
-    rasp_parser::process_resp(test, [](char *buf, size_t len) {
-        __LOG(debug, "buf is : " << (void *)buf << ", len is : " << len);
-    });
-    test = "*2\r\n*3\r\n:1\r\n:2\r\n:3\r\n*2\r\n+Foo\r\n-Bar\r\n";
-    rasp_parser::process_resp(test, [](char *buf, size_t len) {
-        __LOG(debug, "buf is : " << (void *)buf << ", len is : " << len);
-    });
-    test = "*3\r\n$3\r\nfoo\r\n$-1\r\n$3\r\nbar\r\n";
-    rasp_parser::process_resp(test, [](char *buf, size_t len) {
-        __LOG(debug, "buf is : " << (void *)buf << ", len is : " << len);
-    });
+    __LOG(debug, "size of string is : " << test.size() << ", size in the return is : " << std::get<0>(ret));
+    assert(test.size() == std::get<0>(ret));
 
+    test = "*-1\r\n";
+    ret = rasp_parser::process_resp(test, [](char *buf, size_t len) {
+        __LOG(debug, "buf is : " << (void *)buf << ", len is : " << len);
+    });
+    __LOG(debug, "size of string is : " << test.size() << ", size in the return is : " << std::get<0>(ret));
+    assert(test.size() == std::get<0>(ret));
+
+    test = "*3\r\n$3\r\nfoo\r\n$-1\r\n$3\r\nbar\r\n";
+    ret = rasp_parser::process_resp(test, [](char *buf, size_t len) {
+        __LOG(debug, "buf is : " << (void *)buf << ", len is : " << len);
+    });
+    __LOG(debug, "size of string is : " << test.size() << ", size in the return is : " << std::get<0>(ret));
+    assert(test.size() == std::get<0>(ret));
+
+    test = "*3\r\n$3\r\nfoo\r\n$-1\r\n$3\r\nbar\r\n";
+    ret = rasp_parser::process_resp(test, [](char *buf, size_t len) {
+        __LOG(debug, "buf is : " << (void *)buf << ", len is : " << len);
+    });
+    __LOG(debug, "size of string is : " << test.size() << ", size in the return is : " << std::get<0>(ret));
+    assert(test.size() == std::get<0>(ret));
+
+    test = "+OK\r\n+OK\r\n+OK\r\n*3\r\n$3\r\nfoo\r\n$-1\r\n$3\r\nbar\r\n*3\r\n$3\r\nfoo\r\n$-1\r\n$3\r\nbar\r\n";
+    ret = rasp_parser::process_resp(test, [](char *buf, size_t len) {
+        __LOG(debug, "buf is : " << (void *)buf << ", len is : " << len);
+    });
+    __LOG(debug, "size of string is : " << test.size() << ", size in the return is : " << std::get<0>(ret));
+    assert(test.size() == std::get<0>(ret));
 
     getchar();
 }
--- src/resp_parser.cpp
+++ src/resp_parser.cpp
@@ -56,7 +56,7 @@ std::pair<size_t, ParseResult> rasp_parser::process_bulk(std::string buf, size_t
         int bulk_len = 0;
         try
         {
-            std::stoi(buf.substr(index + 1, found - index - 1));
+            bulk_len = std::stoi(buf.substr(index + 1, found - index - 1));
         }
         catch (const std::invalid_argument &ia)
         {
@@ -70,7 +70,7 @@ std::pair<size_t, ParseResult> rasp_parser::process_bulk(std::string buf, size_t
         {
             return std::make_pair(0, Error);
         }
-
+        __LOG(debug, "the bulk len is : " << bulk_len);
         if (bulk_len == -1)
         {
             return std::make_pair(found - index + 2, Completed);
@@ -100,14 +100,16 @@ std::pair<size_t, ParseResult> rasp_parser::process_bulk(std::string buf, size_t
 
 std::pair<size_t, ParseResult> rasp_parser::process_array(std::string buf, size_t index)
 {
-    __LOG(debug, "now process array");
+
     std::size_t found = buf.find("\r\n", index);
+    size_t _sub_index = found + 2;
     if (found != std::string::npos)
     {
         int array_size = 0;
         try
         {
-            std::stoi(buf.substr(index + 1, found - index - 1));
+            array_size = std::stoi(buf.substr(index + 1, found - index - 1));
+            __LOG(debug, "now process array, array size is : " << array_size);
         }
         catch (const std::invalid_argument &ia)
         {
@@ -132,7 +134,6 @@ std::pair<size_t, ParseResult> rasp_parser::process_array(std::string buf, size_
         }
         else
         {
-            size_t _sub_index = found + 2;
             for (int i = 0; i < array_size; i++)
             {
                 auto result = process_resp(buf, nullptr, _sub_index);
@@ -142,9 +143,12 @@ std::pair<size_t, ParseResult> rasp_parser::process_array(std::string buf, size_
                 }
                 else
                 {
+                    __LOG(debug, "now subindex is : " << _sub_index << ", will add : " << std::get<0>(result));
                     _sub_index += std::get<0>(result);
                 }
             }
+            __LOG(debug, "total index is : " << _sub_index);
+            return std::make_pair((_sub_index - index), Completed);
         }
     }
     else
@@ -157,7 +161,7 @@ std::pair<size_t, ParseResult> rasp_parser::process_resp(std::string buf, std::f
     size_t processed_buf = index;
     while (buf.size() > processed_buf)
     {
-        __LOG(debug, "now process "<<std::string((char*)&buf[processed_buf], 1));
+        __LOG(debug, "now process " << std::string((char *)&buf[processed_buf], 1));
         std::pair<size_t, ParseResult> result;
         switch (buf[processed_buf])
         {
@@ -187,16 +191,21 @@ std::pair<size_t, ParseResult> rasp_parser::process_resp(std::string buf, std::f
         }
         break;
         default:
-            return std::make_pair(processed_buf, Error);
+            __LOG(error, "unknown char, not in : + - : $ *, char is : " << std::string((char *)&buf[processed_buf], 1));
+            return std::make_pair(processed_buf - index, Error);
         }
         if (std::get<1>(result) != Completed)
         {
-            return std::make_pair(processed_buf, std::get<1>(result));
+            return std::make_pair(processed_buf - index, std::get<1>(result));
         }
         else
         {
-            on_resp_cb(((char *)buf.data() + processed_buf), std::get<0>(result));
+            if (on_resp_cb)
+            {
+                on_resp_cb(((char *)buf.data() + processed_buf), std::get<0>(result));
+            }
             processed_buf += std::get<0>(result);
         }
     }
+    return std::make_pair(processed_buf - index, Completed);
 }
